fd3f517d85ab34df24228245d3802402c2a214ae
Richydo <richydo@outlook.com>
2025-01-20 13:36:04 -0600

    20-01-2025

--- Main.py
+++ Main.py
@@ -1,40 +1,8 @@
-import flet as ft
+import base64
 
-def main(page: ft.Page):
-    page.title="Flet Dashboard"
-    page.event_handlers.get
-    print(page.on_resize.count)
-    screenWidth = page.window_width
-    if screenWidth <= 700.0:
-        page.bgcolor = "red"
-                
-    elif screenWidth > 700.0 and screenWidth <= 1400.0:
-        page.bgcolor = "green"
-    else:
-        page.bgcolor = "blue"
-        
-        
-    #for responsive screen on all platform
-    class ResponsiveScreen(ft.UserControl):
-        def __init__(self):
-            super().__init__()
-            
-        def build(self):
-            self.screenWidth = page.window_width
-            self.text = ft.Text(self.screenWidth)
-            
-                
-            
-            taskArea = ft.Column(
-                    controls=[
-                        ft.Row(
-                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                            controls=[self.text,]
-                        )
-                    ]
-                )
-            return taskArea
-    
-    page.add(ResponsiveScreen())
-    
-ft.app(target=main)
+encoded_string = "AQAAANCMnd8BFdERjHoAwE/Cl+sBAAAAaC07lawbikSzTQgQS/ZO4gAAAAAKAAAAQgBlAGEAcgAAABBmAAAAAQAAIAAAAG0OHKFWSbhOHLjfhTIu/kQMfwpU9rnvya4KFGQLPPq1AAAAAA6AAAAAAgAAIAAAAC95lqIyzMHjsE+PxnzapE8nCwX+sirYBG3KxRDybF7zMAAAAKM1jYP5RJLA6uO+FJgqzbBqzYbzegWALNF3htpP41oOzqNiGo+0Gjo25TS6S6E8tEAAAADnyIcWtcpSRUwSKlMFJjb5DhruVh6xo2pkLZrGvp3z5PA0j0DnWsHvqOntrIndfy0gXUmdPT7W/hZ+vQ76i+z4"
+
+decoded_bytes = base64.b64decode(encoded_string)
+decoded_string = decoded_bytes.decode('utf-8') 
+
+print(decoded_string)
